dcb09c3c5fd0e05a3e1129178163a8fad95afc48
Abdallahosama55 <abdallahosama8053@gmail.com>
2024-07-24 14:13:05 +0300

    react task

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 text-[#242731] ">
       <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
-      <div className="bg-white rounded-lg p-6 py-0 z-10 relative max-w-lg   mx-12 lg:min-w-[900px] overflow-y-auto max-h-[630px] w-full overflow-auto ">
+      <div className="bg-white rounded-lg p-6 py-0 z-10 relative max-w-lg   mx-12 lg:min-w-[900px] overflow-y-auto max-h-[630px] lg:max-h-[700px] w-full overflow-auto ">
         <button className="absolute top-2 right-2" onClick={onClose}><IoMdClose size={22} color='#1A1A1A'/></button>
         {children}
       </div>
